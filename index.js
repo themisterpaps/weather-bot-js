9ffc7a4cafcf42aa562c96f5084ebda236487be8
Henrique Mateus Chigumane <henriquechigumane@outlook.com>
2020-03-30 23:29:04 +0200

    Change units to metrics and Lang

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const weatherURL = new URL('https://api.openweathermap.org/data/2.5/weather')
 
 weatherURL.searchParams.set('id', '1040652')
 weatherURL.searchParams.set('APPID', weatherToken)
-weatherURL.searchParams.set('units', 'imperial')
+weatherURL.searchParams.set('units', 'metric')
 
 const getWeatherData = async () => {
   const resp = await fetch(weatherURL.toString())
@@ -15,7 +15,7 @@ const getWeatherData = async () => {
   return body
 }
 const generateWeatherMessage = weatherData =>
-  `The weather in ${weatherData.name}: ${weatherData.weather[0].description}. Current temperature is ${weatherData.main.temp}, with a low temp of ${weatherData.main.temp_min} and high of ${weatherData.main.temp_max}.`
+  `O Estado do tempo em ${weatherData.name}: ${weatherData.weather[0].description}°C \n Temperatura é ${weatherData.main.temp}°C \n Min: ${weatherData.main.temp_min}°C \n Max: ${weatherData.main.temp_max}°C`
 
 const main = async () => {
   const weatherData = await getWeatherData()
